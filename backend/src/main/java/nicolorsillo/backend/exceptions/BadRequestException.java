17854ff36c6a23fd917ff9dd930422e5a17a262d
nicolorsillo <nicolorsillo@hotmail.it>
2026-08-05 09:30:00 +0200

    Eccezioni custom e gestione errori

---
+++ backend/src/main/java/nicolorsillo/backend/exceptions/BadRequestException.java
@@ -0,0 +1,7 @@
+package nicolorsillo.backend.exceptions;
+
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
---
+++ backend/src/main/java/nicolorsillo/backend/exceptions/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package nicolorsillo.backend.exceptions;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
