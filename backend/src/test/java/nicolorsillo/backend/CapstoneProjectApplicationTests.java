00891b281205c33d45ba260208259d69451292b3
nicolorsillo <nicolorsillo@hotmail.it>
2026-08-24 09:20:00 +0200

    Test di avvio del contesto Spring

---
+++ backend/src/test/java/nicolorsillo/backend/CapstoneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package nicolorsillo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
